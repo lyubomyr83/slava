10509574d4a9fd112e7f0f81d785ae9992285789
slava <12345>
2018-05-26 18:25:30 +0300

    Добавлен 9.png

--- app/classes/Db.php
+++ app/classes/Db.php
@@ -26,8 +26,8 @@ class Db extends Config
     public static function getInstance()
     {
         if (self::$instance === null)
-        {
-           self::$instance = new self;
+
+           self::$instance = new self;{
         }
 
         return self::$instance;

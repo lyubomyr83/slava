5f3d0eae303ee1c9da3b58f00319204335c250c4
slava <12345>
2018-12-11 14:39:05 +0300

    Редактировано

--- admin/views/VpageList.php
+++ admin/views/VpageList.php
@@ -6,8 +6,8 @@ foreach ($pageslist as $value)
     ?>
     <div id='page' class="row"> 
         <div class="col-md-2"><a href="index.php?page=edit&id=<?=$value['id']?>"><?=$value['menu_name']?></a></div>
-        <div class="col-md-2"><a href="index.php?page=edit&id=<?=$value['id']?>">Редактировать</a></div>
-        <div class="col-md-2"><a href="index.php?page=delete&id=<?=$value['id']?>">Удалить</a></div>
+        <div class="col-md-2"><a href="index.php?page=edit&id=<?=$value['id']?>"><img src="../img/r.png" title="Редактировать"></a></div>
+        <div class="col-md-2"><a href="index.php?page=delete&id=<?=$value['id']?>"><img src="../img/u.png" title="Удалить"></a></div>
         <div class="col-md-2"><?php echo date("d.m.Y \в H:i:s",$value['created']);?></div>
         <div class="col-md-2">
         <?php

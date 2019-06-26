98f82aff377c731fe615298f942c1b8097e89f6e
slava <12345>
2019-06-26 16:02:32 +0300

    1

--- router.php
+++ router.php
@@ -20,31 +20,30 @@ if($_GET)
 
            foreach ($posts as $post_item )
            {
-            ?>
-            <div id="news" class="row">
-                <div class="col-md-3 column"><img src='img/<?=$post_item['image']?>'></div>
-                <div class="col-md-6 content next">
-                    <h2><?=$post_item['post_header']?></h2>
-                    <?php
-                        $content = substr($post_item['post_content'],0,2000);
-                        $content = trim_to_dot($content);
-                    ?>
-                    <?=$content?>
-                    <div class="row">
-                        <div class="col-md-4 content"><a href="?news=#">далее</a></div>
-                        <div class="col-md-4 col-md-offset-4"><span class="glyphicon glyphicon-user"> <?=$post_item['author'] ?></span><span class="glyphicon glyphicon-time"> <?=date("d.m.Y",$post_item['created_at'] )?></span></div>
-                    </div><?php
-                foreach ($categories as $category)
-                    {
-                    echo "<div class=\"col-md-3 column\">
-                    <?={$category['name']};
-                    ?></div>";
-                    }
-                ?>
-                </div>
-            </div>
 
-            <?php
+                    ?>
+                    <div id="news" class="row">
+                    <div class="col-md-3 column"><img src='img/<?=$post_item['image']?>'></div>
+                    <div class="col-md-6 content next">
+                        <h2><?=$post_item['post_header']?></h2>
+                        <?php
+                            $content = substr($post_item['post_content'],0,2000);
+                            $content = trim_to_dot($content);
+                        ?>
+                        <?=$content?>
+                        <div class="row">
+                            <div class="col-md-4 content"><a href="?news=#">далее</a></div>
+                            <div class="col-md-4 col-md-offset-4"><span class="glyphicon glyphicon-user"> <?=$post_item['author'] ?></span><span class="glyphicon glyphicon-time"> <?=date("d.m.Y",$post_item['created_at'] )?></span></div>
+                        </div>
+                    </div>
+                        <div class="col-md-3 column">
+                            <?php
+                            echo "1";
+                            ?>
+                        </div>
+                    </div>
+                    <?php
+
            }
         }
     }

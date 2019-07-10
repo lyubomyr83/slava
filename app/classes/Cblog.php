9836be94e0d590cb6406ce7b4f50386b96c1c14a
lyubomyr83 <lyubomyr83@mail.ru>
2019-07-10 19:31:51 +0300

    categories

--- app/classes/Cblog.php
+++ app/classes/Cblog.php
@@ -6,16 +6,45 @@ class Cblog extends Mblog
 {
     public function getBlog($page_id, $categories=null)
     {
-        if ($categories)
-            {
-                $blog = $this->prepareBlog($page_id, $categories);
-            }
-        else
-            {
-                $blog = $this->prepareBlog($page_id);
-            }
+        echo "<PRE>";
+        var_dump($categories);
+        echo "</PRE>";
+        $blog = $this->prepareBlog($page_id);
         $result = $blog->fetchAll();
-        return $result;
+
+        if ($categories)
+        {
+            foreach ($result as $item)
+            {
+                foreach ($item as $k=>$v)
+                {
+                    if ($k == 'category')
+                    {
+                        $ct_unserialized = unserialize($v);
+                        foreach ($ct_unserialized as $ct_id)
+                        {
+                            if (array_search('{$ct_id}',$categories))
+                            {
+                                // если десерелизованная категория есть в POST со списком выбранных категорий
+
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            foreach ($result['category'])
+
+
+        }
+        else
+        {
+
+        }
+
+
+        return $blog_items;
     }
 
     public function getCategoryList()
--- app/classes/Mblog.php
+++ app/classes/Mblog.php
@@ -4,24 +4,13 @@ namespace app\classes;
 
 class Mblog
 {
-    public function prepareBlog($page_id, $categories=null)
+    public function prepareBlog($page_id)
     {
-        if ($categories)
-        {
-            foreach ($categories as $category)
-                {
-                    $sql = "SELECT * FROM blog WHERE page_id='{$page_id}' AND category='{$category}' ";
-                    $result = Db::getInstance()->read($sql);
-                    return $result;
-                }
-        }
-        else
-            {
-                $sql = "SELECT * FROM blog WHERE page_id='{$page_id}'";
-                $result = Db::getInstance()->read($sql);
-                return $result;
-            }
+        $sql = "SELECT * FROM blog WHERE page_id='{$page_id}'";
+        $result = Db::getInstance()->read($sql);
+        return $result;
     }
+
     public function prepareCategorylist()
     {
         $sql = "SELECT * FROM categories";
